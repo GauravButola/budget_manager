bbaec997affb351fc406efb454305f1f79625cc2
Gaurav Butola <GauravButola@gmail.com>
2014-03-06 13:06:37 +0530

    configured login/register urls and views

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+	return render(request, 'budget/home.html')
+
+def login(request):
+	return HttpResponse("Login here")
+
+def register(request):
+	return HttpResponse("Register here")
