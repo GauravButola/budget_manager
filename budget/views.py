fba97511eea3855448a5c178c404f3a9f5802a52
Gaurav Butola <GauravButola@gmail.com>
2014-03-06 17:15:11 +0530

    Added user registration functionality

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
 	return render(request, 'budget/home.html')
@@ -8,4 +12,15 @@ def login(request):
 	return HttpResponse("Login here")
 
 def register(request):
-	return HttpResponse("Register here")
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/login/')
+
+	args = {}
+	args.update(csrf(request))
+
+	args['form'] = UserCreationForm()
+	return render_to_response('budget/register.html', args)
+
